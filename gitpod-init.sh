709813f8ecf09b7a484571ac5f57ef6c61d92a33
francyorange <francyorange233@gmail.com>
2024-11-13 06:39:15 +0000

    fix db initialization on gitpod

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -2,13 +2,7 @@
 
 sudo apt update && sudo apt install mariadb-server -y && sudo service mariadb start
 
-sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | mariadb-secure-installation
-      # current root password (emtpy after installation)
-    y # Set root password?
-    password # new root password
-    password # new root password
-    y # Remove anonymous users?
-    y # Disallow root login remotely?
-    y # Remove test database and access to it?
-    y # Reload privilege tables now?
-EOF
+sudo mariadb -sfu root <<EOS
+GRANT ALL PRIVILEGES on *.* to 'root'@'localhost' IDENTIFIED BY 'password';
+FLUSH PRIVILEGES
+EOS
